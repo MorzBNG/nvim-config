6056643d2dc543e9d8674dedac79ba6a70e95f1f
MorzBNG <morzbng@gmail.com>
2026-06-11 13:12:36 +0200

    opts change

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,12 +7,14 @@ vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
 vim.opt.wrap = true
+vim.opt.showbreak = "> "
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.swapfile = true
 vim.opt.writebackup = true
+vim.opt.undofile = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
--- lua/plugins/Dashboard.lua
+++ lua/plugins/Dashboard.lua
@@ -37,7 +37,7 @@ return {
                     --{ action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
                     { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
                     --{ action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-                    { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
+                    { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "L" },
                     { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Quit",            icon = " ", key = "q" },
                 },
                 footer = function()
